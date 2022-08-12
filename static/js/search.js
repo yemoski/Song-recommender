13908de81236023abb0776d602e51f0d094b402f
yemoski <yemisiogungbemi89@gmail.com>
2022-08-11 18:22:43 -0700

    'color'

--- static/js/search.js
+++ static/js/search.js
@@ -1,3 +1,12 @@
+
+
+var href = document.getElementsByClassName("play")[0].parentNode.parentNode.href;
+const play_button = document.getElementById('play_button');
+
+/*play_button.addEventListener("click", () => {
+  alert(href);
+});
+
 document.getElementById("submit_button").addEventListener("click", () => {
   document.getElementById("result_container").focus();
-});
+});*/
