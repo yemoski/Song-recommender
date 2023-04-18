bf6d06243ed337c2339f73469ab3f9e1c4b3b026
yemoski <yemisiogungbemi89@gmail.com>
2023-04-18 10:42:52 -0700

    enjoy

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-client_id = 'a5f27ad6dc1a4acabff90a9475c715f4'
-client_secret = 'd81e58a20a5c4e558c614a04aae40361'
+import os 
+
+client_id = os.environ['client_id']
+client_secret = os.environ['client_secret']
